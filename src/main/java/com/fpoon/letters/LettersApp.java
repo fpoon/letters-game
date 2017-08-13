16dd79601a1d173fc15e289022f16cf3434d0845
Mariusz Rebandel <mariusz.rebandel@silksh.pl>
2017-08-13 19:22:40 +0200

    Initial commit

---
+++ src/main/java/com/fpoon/letters/LettersApp.java
@@ -0,0 +1,11 @@
+package com.fpoon.letters;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class LettersApp {
+    public static void main(String[] args) {
+        //TODO: The Whole App
+    }
+}
